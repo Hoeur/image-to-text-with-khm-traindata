82b43c7d5108643a05053d62908102b089888767
Hoeur Lihour <hourofficial359@gmail.com>
2025-01-25 16:19:12 +0700

    update lang khm and eng

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 data = Image.open('image.png')
-text = tess.image_to_string(data, lang='khm')
+text = tess.image_to_string(data, lang='khm+eng')
 
 with open("title.txt", "w", encoding="utf-8") as file:
     file.write(text)
